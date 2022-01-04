7c9ad0ab452f73b18d48376de13f89ad17fdbe44
crobbins327 <crobbins327@gmail.com>
2022-01-04 04:51:54 +0000

    rescale MPP

--- dataset.py
+++ dataset.py
@@ -178,6 +178,9 @@ class WSICoordDataset(Dataset):
         max_coord_per_wsi = 'inf',
         resolution      = 256, # Ensure specific resolution.
         desc = None,
+        rescale_mpp = False,
+        desired_mpp = 0.25,
+        check_white_black = False,
         **super_kwargs,         # Additional arguments for the Dataset base class.
     ):
         self.wsi_dir = wsi_dir
@@ -188,15 +191,17 @@ class WSICoordDataset(Dataset):
             self.process_list = None
         else:
             self.process_list = pd.read_csv(process_list)
-        
         self.patch_size = resolution
+        self.rescale_mpp = rescale_mpp
+        self.desired_mpp = desired_mpp
+        self.check_white_black = check_white_black
         #Implement labels here..
-        self.coord_dict, self.wsi_names = self.createCoordDict(self.wsi_dir, self.wsi_exten, self.coord_dir, self.max_coord_per_wsi, self.process_list, self.patch_size)        
+        self.coord_dict, self.wsi_names, self.wsi_props = self.createCoordDict()
+        
         if desc is None:
             name = str(self.coord_dir)
         else:
             name = desc
-        
         self.coord_size = len(self.coord_dict)  # get the size of coord dataset
         print('Number of WSIs:', len(self.wsi_names))
         print('Number of patches:', self.coord_size)
@@ -205,84 +210,153 @@ class WSICoordDataset(Dataset):
         
         self._all_fnames = os.listdir(self.wsi_dir)
         
-        raw_shape = [self.coord_size] + list(np.array(self._load_raw_image(0)).shape)
+        raw_shape = [self.coord_size] + list(self._load_raw_image(0).shape)
         print('Raw shape of dataset:', raw_shape)
-        if resolution is not None and (raw_shape[1] != resolution or raw_shape[2] != resolution):
+        if resolution is not None and (raw_shape[2] != resolution or raw_shape[3] != resolution):
             raise IOError('Image files do not match the specified resolution')
         super().__init__(name=name, raw_shape=raw_shape, **super_kwargs)
     
-    @staticmethod
-    def createCoordDict(wsi_dir, wsi_exten, coord_dir, max_coord_per_wsi, process_list, patch_size):
-        if process_list is None:
+    def createCoordDict(self):
+        if self.process_list is None:
             #Only use WSI that have coord files....
-            all_coord_files = sorted([x for x in os.listdir(coord_dir) if x.endswith('.h5')])
+            all_coord_files = sorted([x for x in os.listdir(self.coord_dir) if x.endswith('.h5')])
         else:
             #Only use WSI that coord files aren't excluded and are in coord_dir
-            wsi_plist = list(process_list.loc[~process_list['exclude_ids'].isin(['y','yes','Y']),'slide_id'])
-            coord_plist = sorted([x.split(wsi_exten)[0]+'.h5' for x in wsi_plist])
-            all_coord_files = sorted([x for x in os.listdir(coord_dir) if x.endswith('.h5') and x in coord_plist])
+            wsi_plist = list(self.process_list.loc[~self.process_list['exclude_ids'].isin(['y','yes','Y']),'slide_id'])
+            coord_plist = sorted([x.split(self.wsi_exten)[0]+'.h5' for x in wsi_plist])
+            all_coord_files = sorted([x for x in os.listdir(self.coord_dir) if x.endswith('.h5') and x in coord_plist])
         #Get WSI filenames from path that have coord files/in process list
-        wsi_names = sorted([w for w in os.listdir(wsi_dir) if w.endswith(wsi_exten) and w.split(wsi_exten)[0]+'.h5' in all_coord_files])
+        wsi_names = sorted([w for w in os.listdir(self.wsi_dir) if w.endswith(self.wsi_exten) and w.split(self.wsi_exten)[0]+'.h5' in all_coord_files])
                 
         #Get corresponding coord h5 files using WSI paths
-        h5_names = [wsi_name.split(wsi_exten)[0]+'.h5' for wsi_name in wsi_names]
+        h5_names = [wsi_name.split(self.wsi_exten)[0]+'.h5' for wsi_name in wsi_names]
         #Loop through coord files, get coord length, randomly choose X coords for each wsi (max_coord_per_wsi)
         coord_dict = {}
+        wsi_props = {}
         wsi_number = 0
         for h5, wsi_name in zip(h5_names, wsi_names):
             #All h5 paths must exist....
-            h5_path = os.path.join(coord_dir, h5)
+            h5_path = os.path.join(self.coord_dir, h5)
             with h5py.File(h5_path, "r") as f:
+                attrs = dict(f['coords'].attrs)
+                seg_level = attrs['patch_level']
+                dims = attrs['downsampled_level_dim']
+                #patch_size = attrs['patch_size']
                 dset = f['coords']
                 max_len = len(dset)
-                if max_len < float(max_coord_per_wsi):
+                if max_len < float(self.max_coord_per_wsi):
                     #Return all coords
                     coords = dset[:]
                 else:
                     #Randomly select X coords
-                    rand_ind = np.sort(random.sample(range(max_len), int(max_coord_per_wsi)))
+                    rand_ind = np.sort(random.sample(range(max_len), int(self.max_coord_per_wsi)))
                     coords = dset[rand_ind]
             #Check that coordinates and patch resolution is within the dimensions of the WSI... slow but only done once at beginning
-            wsi = openslide.OpenSlide(os.path.join(wsi_dir, wsi_name))
+            if self.check_white_black or self.rescale_mpp:
+                wsi = openslide.OpenSlide(os.path.join(self.wsi_dir, wsi_name))
+
             #Get the desired seg level for the patching based on process list
-            if process_list is not None:
-                seg_level = process_list.loc[process_list['slide_id']==wsi_name,'seg_level'].iloc[0]
+            mpp = None
+            if self.process_list is not None:
+                seg_level = self.process_list.loc[self.process_list['slide_id']==wsi_name,'seg_level'].iloc[0]
+                if self.rescale_mpp and 'MPP' in self.process_list.columns:
+                    mpp = float(self.process_list.loc[self.process_list['slide_id']==wsi_name,'MPP'].iloc[0])
                 #if seg_level != 0:
                 #    print('{} for {}'.format(seg_level, wsi_name))
-            else:
-                seg_level = 0
-                
-            dims = wsi.level_dimensions[seg_level]
+            if self.rescale_mpp and mpp is None:
+                try:
+                    mpp = float(wsi.properties['openslide.mpp-x'])
+                except Exception as e:
+                    print(e)
+                    print(wsi_name)
+                    raise ValueError('Cannot find slide MPP from process list ["MPP"] or Openslide properties. Set rescale_mpp to False to avoid this error or add slide MPPs to process list')
+                            
+            del_index = []
             # print(wsi_name)
             for i,coord in enumerate(coords):
                 #Check that coordinates are inside dims
                 changed = False
             #   old_coord = coord.copy()
-                if coord[0]+patch_size > dims[0]:
-                    coord[0] = dims[0]-patch_size
+                if coord[0]+self.patch_size > dims[0]:
+                    coord[0] = dims[0]-self.patch_size
                 #   print('X not in bounds, adjusting')
                     changed = True
-                if coord[1]+patch_size > dims[1]:
-                    coord[1] = dims[1]-patch_size
+                if coord[1]+self.patch_size > dims[1]:
+                    coord[1] = dims[1]-self.patch_size
                 #   print('Y not in bounds, adjusting')
                     changed = True
                 if changed:
                 #   print("Changing coord {} to {}".format(old_coord, coord))
                     coords[i] = coord
+                # if self.check_white_black:
+                    # patch = np.array(wsi.read_region(coord, seg_level, (self.patch_size, self.patch_size)).convert('RGB'))
+                    #print('Checking if patch is white or black...')
+                    # if isBlackPatch_S(patch, rgbThresh=20, percentage=0.05) or isWhitePatch_S(patch, rgbThresh=220, percentage=0.5):
+                        #print('Removing coord because patch is black or white...')
+                        #print(i)
+                        # del_index.append(i)
+            
+            # if len(del_index) > 0:
+                # print('Removing {} coords that have black or white patches....'.format(len(del_index)))
+                # coords = np.delete(coords, del_index, axis=0)    
             
             #Store as dictionary with tuples {0: (coord, wsi_number), 1: (coord, wsi_number), etc.}
             dict_len = len(coord_dict)
             for i in range(coords.shape[0]):
                 coord_dict[i+dict_len] = (coords[i], wsi_number)
-            
+            wsi_props[wsi_name] = (seg_level, mpp)
             #Storing number/index because smaller size than string
             wsi_number += 1
             
-        return coord_dict, wsi_names 
+        return coord_dict, wsi_names, wsi_props 
     
+    @staticmethod    
+    def adjPatchOOB(wsi_dim, coord, patch_size):
+        #wsi_dim = (wsi_width, wsi_height)
+        #coord = (x, y) with y axis inverted or point (0,0) starting in top left of image
+        #patchsize = integer for square patch only
+        #assume coord starts at (0,0) in line with original WSI,
+        #therefore the patch is only out-of-bounds if the coord+patchsize exceeds the WSI dimensions
+        #check dimensions, adjust coordinate if out of bounds
+        coord = [int(coord[0]), int(coord[1])] 
+        if coord[0]+patch_size > wsi_dim[0]:
+            coord[0] = int(wsi_dim[0] - patch_size)
+        
+        if coord[1]+patch_size > wsi_dim[1]:
+            coord[1] = int(wsi_dim[1] - patch_size) 
+        
+        return tuple(coord)
+
+    def scalePatch(self, wsi, coord, input_mpp=0.5, desired_mpp=0.25, patch_size=512, eps=0.05, level=0):
+        desired_mpp = float(desired_mpp)
+        input_mpp = float(input_mpp)
+        factor = desired_mpp/input_mpp
+        #Openslide get dimensions of full WSI
+        dims = wsi.level_dimensions[0]
+        if input_mpp > desired_mpp + eps or input_mpp < desired_mpp - eps:
+            #print('scale by {:.2f} factor'.format(factor))
+            # if factor > 1
+            #input mpp must be smaller and therefore at higher magnification (e.g. desired 40x vs input 60x) and vice versa
+            #approach: shrink a larger patch by factor to the desired patch size or enlarge a smaller patch to desired patch size
+            scaled_psize = int(patch_size*factor)
+            #check and adjust dimensions of coord based on scaled patchsize
+            coord = self.adjPatchOOB(dims, coord, scaled_psize)
+            adj_patch = wsi.read_region(coord, level, (scaled_psize, scaled_psize)).convert('RGB')
+            #shrink patch down to desired mpp if factor > 1
+            #enlarge if factor < 1
+            # patch = cv2.resize(adj_patch, (patch_size, patch_size), interpolation=cv2.INTER_LINEAR)
+            patch = adj_patch.resize((patch_size, patch_size), Image.BILINEAR)
+            return patch
+        else: 
+            #print('skip scaling factor {:.2f}. input um per pixel ({}) within +/- {} of desired MPP ({}).'.format(factor, input_mpp, eps, desired_mpp))
+            coord = self.adjPatchOOB(dims, coord, patch_size)
+            patch = wsi.read_region(coord, level, (patch_size, patch_size)).convert('RGB')
+            return patch
+            
     def _load_raw_image(self, raw_idx):
         coord, wsi_num = self.coord_dict[raw_idx % self.coord_size]
         wsi_name = self.wsi_names[wsi_num]
+        seg_level, mpp = self.wsi_props[wsi_name]
         #print('opening {}'.format(wsi_name))
         img_path = os.path.join(self.wsi_dir, wsi_name)
         wsi = openslide.OpenSlide(img_path)
@@ -291,15 +365,17 @@ class WSICoordDataset(Dataset):
         # if self.wsi_open is None or self.wsi_open != self.wsi_names[wsi_num]:
             # self.wsi = openslide.OpenSlide(img_path)
             # self.wsi_open = self.wsi_names[wsi_num]
-        if self.process_list is not None:
-            seg_level = self.process_list.loc[self.process_list['slide_id']==wsi_name,'seg_level'].iloc[0]
-            #if seg_level != 0:
-            #    print('{} for {}'.format(seg_level, wsi_name))
+        if self.rescale_mpp:
+            if mpp is None:
+                try:
+                    mpp = wsi.properties['openslide.mpp-x']
+                except Exception as e:
+                    print(e)
+                    print(wsi_name)
+                    raise ValueError('Cannot find slide MPP from process list or Openslide properties. Set rescale_mpp to False to avoid this error or add slide MPPs to process list.')
+            img = self.scalePatch(wsi=wsi, coord=coord, input_mpp=mpp, desired_mpp=self.desired_mpp, patch_size=self.patch_size, level=seg_level) 
         else:
-            seg_level = 0
-        img = wsi.read_region(coord, seg_level, (self.patch_size, self.patch_size)).convert('RGB')
-        # img = img.transpose(2, 0, 1) # HWC => CHW
-        # img = np.moveaxis(img, 2, 0) # HWC => CHW
+            img = wsi.read_region(coord, seg_level, (self.patch_size, self.patch_size)).convert('RGB')
         return img
     
     # def __getstate__(self):
--- train-110.py
+++ train-110.py
@@ -343,6 +343,7 @@ if __name__ == "__main__":
     parser.add_argument("--process_list", type=str, default=None, help="WSI process list and seg levels [default None]")
     parser.add_argument("--wsi_exten", type=str, default='.tif', help="WSI extension [default '.tif']")
     parser.add_argument("--max_coord_per_wsi", type=float, default='inf', help="Maximum number of coordinates to sample from WSI [default inf]")
+    parser.add_argument("--rescale_mpp", type=float, default=None, help='Rescale WSIs to microns per pixel (MPP)')
     parser.add_argument("--max_size", type=int, default=None, help="Maximum number of images to train with [default inf]")
     parser.add_argument('--arch', type=str, default='stylegan2', help='model architectures (stylegan2 | swagan)')
     parser.add_argument(
@@ -539,8 +540,17 @@ if __name__ == "__main__":
     )
 
     # dataset = MultiResolutionDataset(args.path, transform, args.size)
+    if args.rescale_mpp is not None:
+        assert isinstance(args.rescale_mpp, float)
+        rescale_bool = True
+        desired_mpp = float(args.rescale_mpp)
+    else:
+        rescale_bool = False
+        desired_mpp = None
     dataset = WSICoordDataset(wsi_dir=args.wsi_dir, coord_dir=args.coord_dir, process_list=args.process_list,
-                        wsi_exten=args.wsi_exten, max_coord_per_wsi=args.max_coord_per_wsi, resolution=args.size, desc=args.log_name, 
+                        wsi_exten=args.wsi_exten, max_coord_per_wsi=args.max_coord_per_wsi, resolution=args.size, 
+                        rescale_mpp = rescale_bool, desired_mpp = desired_mpp,
+                        desc=args.log_name, 
                         # use_labels=False,
                         max_size=args.max_size, xflip=False, transform = transform)
     loader = data.DataLoader(
